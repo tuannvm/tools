62c5fa2915ff310f9cbdfc5fac9b73e048549eff
Tommy Nguyen <me@tuannvm.com>
2019-07-24 17:04:23 +0800

    Add mosh
    Signed-off-by: Tommy Nguyen <me@tuannvm.com>

--- cloud-shell/init.sh
+++ cloud-shell/init.sh
@@ -6,4 +6,4 @@ mkdir -p ~/.cloudshell
 touch ~/.cloudshell/no-apt-get-warning
 
 sudo apt-get update
-sudo apt-get install telnet git curl iputils-ping dnsutils traceroute net-tools iproute2 iperf3 postgresql-client netcat nvim -y
+sudo apt-get install telnet git curl iputils-ping dnsutils traceroute net-tools iproute2 iperf3 postgresql-client netcat mosh -y
