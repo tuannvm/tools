7ef6f4ad5d0101942a61b4269cba67c3cf44c3ee
Tommy Nguyen <me@tuannvm.com>
2019-07-24 16:25:48 +0800

    Add cloud shell script
    Signed-off-by: Tommy Nguyen <me@tuannvm.com>

---
+++ cloud-shell/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BIN_PATH=/usr/local/bin
+HELM_VERSION=2.12.2
+KUBECTL_VERSION=v1.13.0
+TERRAFORM_VERSION=0.12.5
+
+apt-get update
+apt-get install telnet git curl iputils-ping dnsutils traceroute net-tools iproute2 iperf3 postgresql-client netcat -y
+
+curl -S -L -f https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz | tar -xzC ${BIN_PATH}/ && \
+    mv $BIN_PATH/linux-amd64/helm $BIN_PATH/ && \
+    rm -rf $BIN_PATH/linux-amd64/
+
+curl -S -L -f https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl -o ${BIN_PATH}/kubectl && \
+    chmod +x ${BIN_PATH}/kubectl
+
+curl -S -L -f https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip | bsdtar -xvf- -C $(BIN_PATH)
