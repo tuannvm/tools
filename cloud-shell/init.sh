f9d5d65337e06e9c22e1fdfd4535d7c28626b602
Tommy Nguyen <me@tuannvm.com>
2019-07-24 16:41:29 +0800

    Change script
    Signed-off-by: Tommy Nguyen <me@tuannvm.com>

--- cloud-shell/init.sh
+++ cloud-shell/init.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+touch ~/.cloudshell/no-apt-get-warning
+
 BIN_PATH=/usr/local/bin
 HELM_VERSION=2.12.2
 KUBECTL_VERSION=v1.13.0
@@ -12,6 +14,8 @@ sudo apt-get install telnet git curl iputils-ping dnsutils traceroute net-tools 
 
 mkdir -p $BIN_PATH
 
+cd /tmp/
+
 curl -S -L -f https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz | tar -xzC ${BIN_PATH}/ && \
     mv $BIN_PATH/linux-amd64/helm $BIN_PATH/ && \
     rm -rf $BIN_PATH/linux-amd64/
@@ -20,3 +24,5 @@ curl -S -L -f https://storage.googleapis.com/kubernetes-release/release/${KUBECT
     chmod +x ${BIN_PATH}/kubectl
 
 curl -S -L -f https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip | bsdtar -xvf- -C $(BIN_PATH)
+
+cd --
