c39bf764c6cb231f7ba64e1aa05645563bc6b709
Tommy Nguyen <me@tuannvm.com>
2019-07-25 12:08:10 +0800

    Add ssh info
    Signed-off-by: Tommy Nguyen <me@tuannvm.com>

--- cloud-shell/init.sh
+++ cloud-shell/init.sh
@@ -7,3 +7,10 @@ touch ~/.cloudshell/no-apt-get-warning
 
 sudo apt-get update
 sudo apt-get install telnet git curl iputils-ping dnsutils traceroute net-tools iproute2 iperf3 postgresql-client netcat mosh -y
+
+cat << EOF >> /etc/ssh/ssh_config
+Host me
+Hostname tuannvm.com
+Port 2220
+User me
+EOF
