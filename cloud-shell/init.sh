74dd75d50024ed6fbe2b370975603f852f46b805
Tommy Nguyen <me@tuannvm.com>
2019-07-25 14:15:03 +0800

    Update the condition
    Signed-off-by: Tommy Nguyen <me@tuannvm.com>

--- cloud-shell/init.sh
+++ cloud-shell/init.sh
@@ -8,9 +8,11 @@ touch ~/.cloudshell/no-apt-get-warning
 sudo apt-get update
 sudo apt-get install telnet git curl iputils-ping dnsutils traceroute net-tools iproute2 iperf3 postgresql-client netcat mosh -y
 
-cat << EOF >> /etc/ssh/ssh_config
-Host me
-Hostname tuannvm.com
-Port 2220
-User me
+if ! grep -qF "Host me" /etc/ssh/ssh_config; then
+  cat << EOF | sudo tee -a /etc/ssh/ssh_config
+  Host me
+  Hostname tuannvm.com
+  Port 2220
+  User me
 EOF
+fi
